728459824b96ca72c9c359a4f764e7153b13a2d8
Nicole Remy <nicoleremy95@gmail.com>
2020-06-24 16:39:05 -0500

    save for pull dev

--- assets/script_beer.js
+++ assets/script_beer.js
@@ -1,38 +1,62 @@
 var city = "seattle";
 var code = "98101";
 var tag = "patio";
-var beerInput = $(input)
+var beerInput = $("#beersearch").val().trim();
+var queryURLBeerCode = "https://api.openbrewerydb.org/breweries?by_postal=" + beerInput
+    console.log(queryURLBeerCode)
+var queryURLBeerCity = "https://api.openbrewerydb.org/breweries?by_city=" + beerInput
+    console.log(queryURLBeerCity)
+// var beerContainer = 
 
 function getBreweries(){
     // if input int run this code Code input === int(input())
     if(beerInput === int(beerInput())){
-    var queryURLBeerCode = "https://api.openbrewerydb.org/breweries?by_postal=" + code
-    console.log(queryURLBeerCode)
-    
     $.ajax({
         url: queryURLBeerCode,
         method: "GET"
     }).then(function(breweries){
             breweries.forEach(function(brewery){
-                var name = brewery.name
+                // var beerCard = 
+                var name = $("<h3>")
+                name.text(brewery.name)
                 console.log(brewery.name)
+                beerCard.append(name)
 
-                var city = brewery.city
+                var city = $("<h3>")
+                city.text(brewery.city)
                 console.log(brewery.city)
+                beerCard.append(city)
 
-                var street = brewery.street
+                var street = $("<h3>")
+                street.text(brewery.street)
                 console.log(brewery.street)
+                beerCard.append(street)
 
                 var url = brewery.website_url
                 console.log(brewery.website_url)
+                beerCard.append(street)
             })
-    })
-    // }
-    //else input is a word run this code City input 
-    // else {
-    var queryURLBeerCity = "https://api.openbrewerydb.org/breweries?by_city=" + city
-    console.log(queryURLBeerCity)
-    // }
+    })  
 }
+    // else {
+    // $.ajax({
+    //     url: queryURLBeerCity,
+    //     method: "GET"
+    // }).then(function(breweries){
+    //         breweries.forEach(function(brewery){
+    //             var name = brewery.name
+    //             console.log(brewery.name)
+
+    //             var city = brewery.city
+    //             console.log(brewery.city)
+
+    //             var street = brewery.street
+    //             console.log(brewery.street)
+
+    //             var url = brewery.website_url
+    //             console.log(brewery.website_url)
+    //         })
+    // })
+    // }
 }
 getBreweries();
