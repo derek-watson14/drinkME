ed6af5d836b2fd2b25a3f37c28cc1155ea0a09ee
Sam Grose <samugrose@gmail.com>
2020-06-25 11:42:55 -0700

    saved changes for pull

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
-var alcohol = "";
+var alcohol = "Vodka";
 var currentDrink = "";
 var thumbnails = [];
 var recipe = "";
@@ -20,7 +20,7 @@ $.ajax({
         currentDrink = response.drinks[i].strDrink; // loops through all 12 drink tiles
         getIngreds();
         var imgUrl = response.drinks[i].strDrinkThumb + "/preview";
-        //console.log(imgUrl);
+        console.log(imgUrl);
         thumbnails.push(imgUrl); //makes first drink appear at index '0' in thumbnails
     }
 });
@@ -57,71 +57,65 @@ $("#cocktailsearch-submit").on("click", function() {
     event.preventDefault();
     console.log("clicked");
     alcohol = $("#cocktailsearch").val();
+    console.log(alcohol + " alcohol val");
     getBooze();
     getIngreds();
 
-    var newDiv = $("<div>"); // append to section with right ID
-    newDiv.addClass("col s12 m7");
-    var newH2 = $("<h2>");
-    newH2.addClass("header");
-    newH2.text("Test");
-    newDiv.append(newH2);
+    var newRow = $("<row>");
+    //for (i = 0; i < 4; i++) {
+        var drinkCard = $("<div class='card-horizontal col s12 m6'>");
+        var newDiv2 = $("<div class='card-image'>");
+        var drinkImg = $("<img src='https://lorempixel.com/100/190/nature/6'>");
+        newDiv2.append(drinkImg);
+        drinkCard.append(newDiv2);
+        // newDiv1.append(newDiv2);
+        // var drinkImg = $("<img>");
+        // drinkImg.attr("src", "https://lorempixel.com/100/190/nature/6");
+        // newDiv2.append(drinkImg);
+        newRow.append(drinkCard);
+   // }
+   var cocktaillist = $("#cocktaillist")
+    $(cocktaillist).append(newRow);
+    // $("#cocktaillist").append(newRow);
+    // var newDiv = $("<div>"); // append to section with right ID
+    // newDiv.addClass("col s12 m4");
+    // var newH2 = $("<h2>");
+    // newH2.addClass("header");
+    // newH2.text("Test");
+    // newDiv.append(newH2);
 
-    var newDiv1 = $("<div>");
-    newDiv1.addClass("card-horizontal");
-    newDiv.append(newDiv1);
+    // var newDiv1 = $("<div>");
+    // newDiv1.addClass("card-horizontal");
+    // newDiv.append(newDiv1);
 
-    var newDiv2 = $("<div>");
-    newDiv2.addClass("card-image");
-    newDiv1.append(newDiv2);
-    var drinkImg = $("<img>");
-    drinkImg.attr("src", "https://lorempixel.com/100/190/nature/6");
-    newDiv2.append(drinkImg);
+    // var newDiv2 = $("<div>");
+    // newDiv2.addClass("card-image");
+    // newDiv1.append(newDiv2);
+    // var drinkImg = $("<img>");
+    // drinkImg.attr("src", "https://lorempixel.com/100/190/nature/6");
+    // newDiv2.append(drinkImg);
 
-    //append below newDiv3 to card-horizontal newDiv1
-    var newDiv3 = $("<div>");
-    newDiv3.addClass("card-stacked");
-    newDiv1.append(newDiv3); //adds "card-stacked" to div, now append card-content and the p and a to newdiv3
+    // //append below newDiv3 to card-horizontal newDiv1
+    // var newDiv3 = $("<div>");
+    // newDiv3.addClass("card-stacked");
+    // newDiv1.append(newDiv3); //adds "card-stacked" to div, now append card-content and the p and a to newdiv3
     
-    var newDiv4 = $("<div>");
-    newDiv4.addClass("card-content");
-    newDiv3.append(newDiv4);
+    // var newDiv4 = $("<div>");
+    // newDiv4.addClass("card-content");
+    // newDiv3.append(newDiv4);
 
-    var newP = $("<p>");
-    newP.text("hello, vodka!");
-    newDiv4.append(newP);
-    //now append another div to card-content, with an a in it
+    // var newP = $("<p>");
+    // newP.text("hello, vodka!");
+    // newDiv4.append(newP);
+    // //now append another div to card-content, with an a in it
 
-    var newDiv5 = $("<div>");
-    newDiv5.addClass("card-action");
-    newDiv3.append(newDiv5);
+    // var newDiv5 = $("<div>");
+    // newDiv5.addClass("card-action");
+    // newDiv3.append(newDiv5);
 
-    var newA = $("<a>");
-    newA.attr("href", "htps://www.drizly.com");
-    newA.text("Drizly it!");
-    newDiv5.append(newA);
-    $("#cocktaillist").append(newDiv);
-<<<<<<< HEAD
-=======
-
-
-//     <div class="col s12 m7">
-//         <h2 class="header">Horizontal Card</h2>
-//         <div class="card horizontal">
-//             <div class="card-image">
-//                 <img src="https://lorempixel.com/100/190/nature/6">
-//             </div>
-//             <div class="card-stacked">
-//                 <div class="card-content">
-//                 <p>I am a very simple card. I am good at containing small bits of information.</p>
-//                 </div>
-//                 <div class="card-action">
-//                 <a href="#">This is a link</a>
-//                 </div>
-//             </div>
-//             </div>
-//   </div>
-    
-    //console.log(alcohol);
->>>>>>> dev
+    // var newA = $("<a>");
+    // newA.attr("href", "htps://www.drizly.com");
+    // newA.text("Drizly it!");
+    // newDiv5.append(newA);
+    // $(newRow).append(newDiv);
 })
